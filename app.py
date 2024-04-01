c9a0f949129011ebc0c577f802c3b91c5f31ee46
B.S.Rohith Kumar <118740438+Rohithkumar77@users.noreply.github.com>
2024-04-01 15:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import streamlit as st
 import subprocess
-import frontend.Home as home
 
 def run_streamlit(file_path):
     command = ["streamlit", "run", file_path]
     subprocess.Popen(command)
 
+def run_backend():
+    # Replace 'backend/main.py' with the actual path to your backend script
+    backend_command = ["python", "backend/main.py"]
+    subprocess.Popen(backend_command)
+
 def main():
-    # Run main.py from the backend folder in a separate Streamlit process
-    run_streamlit("backend/main.py")
+    # Run frontend/Home.py using Streamlit
+    run_streamlit("frontend/Home.py")
 
-    # Run home.py from the frontend folder in the current Streamlit process
-    import frontend.Home as home
-
-    home.main()
+    # Run backend/main.py as a separate process
+    run_backend()
 
 if __name__ == "__main__":
     main()
