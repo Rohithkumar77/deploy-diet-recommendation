111e0ac2b21058d3a7221bb443cae9119dd878a0
B.S.Rohith Kumar <118740438+Rohithkumar77@users.noreply.github.com>
2024-04-13 11:31:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_docker_compose():
     command = ["docker-compose", "up", "-d", "--build"]
-    subprocess.run(command)
+    subprocess.run(command, capture_output=True)
 
 # Call the function to run the command
 run_docker_compose()
